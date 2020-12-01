42f321fc5fce82629f5f77f56a96b05b966b4a5f
Shiro39 <38461122+shirooo39@users.noreply.github.com>
2020-12-01 22:11:54 +0700

    Update mixlab.py

--- resources/mixlab.py
+++ resources/mixlab.py
@@ -513,3 +513,198 @@ class PortForward:
 class PortForward_wrapper(PortForward):
   def __init__(self,SERVICE,TOKEN,USE_FREE_TOKEN,connections,region,config):
     super(self.__class__,self).__init__(connections,region,SERVICE,TOKEN,USE_FREE_TOKEN,config)
+
+# ====================================================================================================
+def installJDownloader():
+	if checkAvailable("/root/.JDownloader/JDownloader.jar"):
+        return
+    else:
+        runSh("mkdir -p -m 666 /root/.JDownloader/libs")
+        runSh("apt-get install openjdk-8-jre-headless -qq -y")
+        runSh(
+            "wget -q http://installer.jdownloader.org/JDownloader.jar -O /root/.JDownloader/JDownloader.jar"
+        )
+        runSh("java -jar /root/.JDownloader/JDownloader.jar -norestart -h")
+        runSh(
+            "wget -q https://biplobsd.github.io/RLabClone/res/jdownloader/sevenzipjbinding1509.jar -O /root/.JDownloader/libs/sevenzipjbinding1509.jar"
+        )
+        runSh(
+            "wget -q https://biplobsd.github.io/RLabClone/res/jdownloader/sevenzipjbinding1509Linux.jar -O /root/.JDownloader/libs/sevenzipjbinding1509Linux.jar"
+        )
+
+Email = widgets.Text(placeholder="*Required", description="Email:")
+Password = widgets.Text(placeholder="*Required", description="Password:")
+Device = widgets.Text(placeholder="Optional", description="Name:")
+SavePath = widgets.Dropdown(
+    value="/content/Downloads",
+    options=["/content", "/content/Downloads"],
+    description="Save Path:",
+)
+
+
+def refreshJDPath(a=1):
+    if checkAvailable("/content/drive/"):
+        if checkAvailable("/content/drive/Shared drives/"):
+            SavePath.options = (
+                ["/content", "/content/Downloads", "/content/drive/My Drive"]
+                + glob("/content/drive/My Drive/*/")
+                + glob("/content/drive/Shared drives/*/")
+            )
+        else:
+            SavePath.options = [
+                "/content",
+                "/content/Downloads",
+                "/content/drive/My Drive",
+            ] + glob("/content/drive/My Drive/*/")
+    else:
+        SavePath.options = ["/content", "/content/Downloads"]
+
+
+def exitJDWeb():
+    runSh("pkill -9 -e -f java")
+    clear_output(wait=True)
+    createButton("Start", func=startJDService, style="info")
+
+
+def confirmJDForm(a):
+    clear_output(wait=True)
+    action = a.description
+    createButton(f"{action} Confirm?")
+    if action == "Restart":
+        createButton("Confirm", func=startJDService, style="danger")
+    else:
+        createButton("Confirm", func=exitJDWeb, style="danger")
+    createButton("Cancel", func=displayJDControl, style="warning")
+
+
+def displayJDControl(a=1):
+    clear_output(wait=True)
+    createButton("Control Panel")
+    display(
+        HTML(
+            """
+            <h3 style="font-family:Trebuchet MS;color:#4f8bd6;">
+                You can login to the WebUI by clicking
+                    <a href="https://my.jdownloader.org/" target="_blank">
+                        here
+                    </a>.
+            </h3>
+            """
+        ),
+        HTML(
+            """
+            <h4 style="font-family:Trebuchet MS;color:#4f8bd6;">
+                If the server didn't showup in 30 sec. please re-login.
+            </h4>
+            """
+        ),
+    )
+    createButton("Re-Login", func=displayJDLoginForm, style="info")
+    createButton("Restart", func=confirmJDForm, style="warning")
+    createButton("Exit", func=confirmJDForm, style="danger")
+
+
+def startJDService(a=1):
+    runSh("pkill -9 -e -f java")
+    runSh(
+        "java -jar /root/.JDownloader/JDownloader.jar -norestart -noerr -r &",
+        shell=True,  # nosec
+    )
+    displayJDControl()
+
+
+def displayJDLoginForm(a=1):
+    clear_output(wait=True)
+    Email.value = ""
+    Password.value = ""
+    Device.value = ""
+    refreshJDPath()
+    display(
+        HTML(
+            """
+            <h3 style="font-family:Trebuchet MS;color:#4f8bd6;">
+                If you don't have an account yet, please register
+                    <a href="https://my.jdownloader.org/login.html#register" target="_blank">
+                        here
+                    </a>.
+            </h3>
+            """
+        ),
+        HTML("<br>"),
+        Email,
+        Password,
+        Device,
+        SavePath,
+    )
+    createButton("Refresh", func=refreshJDPath)
+    createButton("Login", func=startJDFormLogin, style="info")
+    if checkAvailable(
+        "/root/.JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json"
+    ):
+        createButton("Cancel", func=displayJDControl, style="danger")
+
+
+def startJDFormLogin(a=1):
+    try:
+        if not Email.value.strip():
+            ERROR = "Email field is empty."
+            THROW_ERROR
+        if not "@" in Email.value and not "." in Email.value:
+            ERROR = "Email is an incorrect format."
+            THROW_ERROR
+        if not Password.value.strip():
+            ERROR = "Password field is empty."
+            THROW_ERROR
+        if not bool(re.match("^[a-zA-Z0-9]+$", Device.value)) and Device.value.strip():
+            ERROR = "Only alphanumeric are allowed for the device name."
+            THROW_ERROR
+        clear_output(wait=True)
+        if SavePath.value == "/content":
+            savePath = {"defaultdownloadfolder": "/content/Downloads"}
+        elif SavePath.value == "/content/Downloads":
+            runSh("mkdir -p -m 666 /content/Downloads")
+            savePath = {"defaultdownloadfolder": "/content/Downloads"}
+        else:
+            savePath = {"defaultdownloadfolder": SavePath.value}
+
+        with open(
+            "/root/.JDownloader/cfg/org.jdownloader.settings.GeneralSettings.json", "w+"
+        ) as outPath:
+            json.dump(savePath, outPath)
+        if Device.value.strip() == "":
+            Device.value = Email.value
+        runSh("pkill -9 -e -f java")
+        data = {
+            "email": Email.value,
+            "password": Password.value,
+            "devicename": Device.value,
+            "directconnectmode": "LAN",
+        }
+        with open(
+            "/root/.JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json",
+            "w+",
+        ) as outData:
+            json.dump(data, outData)
+        startJDService()
+    except:
+        print(ERROR)
+
+
+def handleJDLogin(newAccount):
+    installJDownloader()
+    if newAccount:
+        displayJDLoginForm()
+    else:
+        data = {
+            "email": "daniel.dungngo@gmail.com",
+            "password": "ZjPNiqjL4e6ckwM",
+            "devicename": "daniel.dungngo@gmail.com",
+            "directconnectmode": "LAN",
+        }
+        with open(
+            "/root/.JDownloader/cfg/org.jdownloader.api.myjdownloader.MyJDownloaderSettings.json",
+            "w+",
+        ) as outData:
+            json.dump(data, outData)
+        startJDService()
+# ====================================================================================================
