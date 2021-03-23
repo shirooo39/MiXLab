c7cf899f6a007c64c9d140efdc5ea4e6cf800cf6
Shiro39 <38461122+shirooo39@users.noreply.github.com>
2021-03-23 13:02:43 +0700

    Restored repository from a previously working state
    Well... sometimes things happen, right? lmao

--- resources/mixlab.py
+++ resources/mixlab.py
@@ -1,6 +1,8 @@
-import os, time, uuid, re, IPython, sys, json, ipywidgets as widgets
+import os, time, uuid, re, IPython, sys
+import json
+import ipywidgets as widgets
 from sys import exit as exx
-from subprocess import Popen, PIPE
+from subprocess import Popen,PIPE
 from google.colab import files  # pylint: disable=import-error
 from glob import glob
 from IPython.display import HTML, display, clear_output
@@ -325,22 +327,6 @@ def findProcess(process, command="", isPid=False):
             except:
                 continue
 
-def installArgoTunnel():
-    if checkAvailable(f"{HOME}/tools/argotunnel/cloudflared"):
-        return
-    else:
-        import os
-        from shutil import unpack_archive
-        from urllib.request import urlretrieve
-        
-        os.makedirs(f'{HOME}/tools/argotunnel/', exist_ok=True)
-        aTURL = findPackageR("cloudflare/cloudflared", "cloudflared-linux-amd64")
-        urlretrieve(aTURL, f'{HOME}/tools/argotunnel/cloudflared')
-        # unpack_archive('cloudflared.tgz',
-        #   f'{HOME}/tools/argotunnel')
-        os.chmod(f'{HOME}/tools/argotunnel/cloudflared', 0o755)
-        # os.unlink('cloudflared.tgz')
-
 def installNgrok():
     if checkAvailable("/usr/local/bin/ngrok"):
         return
@@ -502,7 +488,7 @@ class LocalhostRun:
         accessSettingFile("localhostDB.json" , localhostOpenDB, v=False)
         return newAddr
       except:
-        outs, errs = self.connection.communicate(timeout=15)
+        outs, errs = self.connection.communicate(timeout=30)
         self.connection.kill()
         # print(outs)
         # print(errs)
@@ -515,128 +501,6 @@ class LocalhostRun:
   def kill(self):
     self.connection.kill()
 
-class ArgoTunnel:
-  def __init__(self, port, proto='http', metrics=49589, interval=30, retries=30):
-    import os
-    filePath = "/usr/local/sessionSettings/argotunnelDB.json"
-    if not os.path.exists(filePath):
-      os.makedirs(filePath[:-17], exist_ok=True)
-      open(filePath, 'w').close()
-    
-    #Installing argotunnel
-    installArgoTunnel()
-
-    self.connection=None
-    self.proto=proto
-    self.port=port
-    self.metricPort=metrics
-    self.interval=interval
-    self.retries=retries
-
-  # def start(self):
-  #   if self.connection:self.connection.kill()
-  #   # self.connection=Popen(f"ssh -R 80:localhost:{self.port} {self.id}@ssh.localhost.run -o StrictHostKeyChecking=no".split(), stdout=PIPE, stdin=PIPE)
-  #   self.connection=Popen(f"/content/tools/argotunnel/cloudflared tunnel --url {self.proto}://0.0.0.0:{self.port} --logfile cloudflared.log".split(), stdout=PIPE, stdin=PIPE)
-  #   try:
-  #     return re.findall("https://(.*?.trycloudflare.com)",self.connection.stdout.readline().decode("utf-8"))[0]
-  #   except:
-  #     raise Exception(self.connection.stdout.readline().decode("utf-8"))
-
-  def keep_alive(self):
-    # if self.connection:self.connection.kill()
-    import urllib, requests, re
-    from urllib.error import HTTPError
-    
-    try:
-      argotunnelOpenDB = dict(accessSettingFile("argotunnelDB.json", v=False))
-    except TypeError:
-      argotunnelOpenDB = dict()
-
-    if findProcess("cloudflared", f"localhost:{self.metricPort}"):
-      try:
-        oldAddr = argotunnelOpenDB[str(self.port)]
-        if requests.get("http://"+oldAddr).status_code == 200:
-          return oldAddr
-      except:
-        pass
-
-    self.connection=Popen(f"{HOME}/tools/argotunnel/cloudflared tunnel --url {self.proto}://0.0.0.0:{self.port} --logfile {HOME}/tools/argotunnel/cloudflared.log --metrics localhost:{self.metricPort}".split(),
-      stdout=PIPE, stdin=PIPE, stderr=PIPE, universal_newlines=True)
-    
-    time.sleep(5)
-
-    hostname = None
-    for i in range(20):
-      try:
-        with urllib.request.urlopen(f"http://127.0.0.1:{self.metricPort}/metrics") as response:
-            hostname = re.search(r'userHostname=\"https://(.+)\"',
-             response.read().decode('utf-8'), re.MULTILINE)
-            if not hostname:
-              time.sleep(1)
-              continue
-            hostname = hostname.group(1)
-            break
-      except HTTPError:
-        time.sleep(2)
-        
-    if not hostname:
-      raise RuntimeError("Failed to get user hostname from cloudflared")
-    
-    argotunnelOpenDB[str(self.port)] = hostname
-    accessSettingFile("argotunnelDB.json" , argotunnelOpenDB, v=False)
-    return hostname
-
-  def kill(self):
-    self.connection.kill()
-
-class jprq:
-  def __init__(self, port, proto='http', ids=None):
-    import os, uuid
-    filePath = "/usr/local/sessionSettings/jprqDB.json"
-    if not os.path.exists(filePath):
-      os.makedirs(filePath[:-11], exist_ok=True)
-      open(filePath, 'w').close()
-
-    if not ids:self.ids=str(uuid.uuid4())[:12]
-    #Installing jprq
-    runSh("pip install jprq")
-
-    self.connection=None
-    self.proto=proto
-    self.port=port
-
-  def keep_alive(self):
-    # if self.connection:self.connection.kill()
-    import urllib, requests, re
-    try:
-      jprqOpenDB = dict(accessSettingFile("jprqDB.json", v=False))
-    except TypeError:
-      jprqOpenDB = dict()
-
-    if findProcess("jprq", f"{self.port}"):
-      try:
-        oldAddr = jprqOpenDB[str(self.port)]
-        if requests.get("http://"+oldAddr).status_code == 200:
-          return oldAddr
-      except:
-        pass
-    hostname = f"MiXLab-{self.ids}"
-    self.connection=Popen(f"jprq -s {hostname} {self.port}".split(),
-      stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    
-    time.sleep(3)
-
-    # try:
-    #   return re.findall("https://(.*?.jprq.live/)", self.connection.stdout.readline().decode("utf-8"))[0]
-    # except:
-    #   raise Exception(self.connection.stdout.readline().decode("utf-8"))
-    hostname += ".jprq.live"
-    jprqOpenDB[str(self.port)] = hostname
-    accessSettingFile("jprqDB.json" , jprqOpenDB, v=False)
-    return hostname
-
-  def kill(self):
-    self.connection.kill()
 
 class PortForward:
   def __init__(self,connections,region=None,SERVICE="localhost",TOKEN=None,USE_FREE_TOKEN=None,config=None):
@@ -644,11 +508,8 @@ class PortForward:
     for con in connections:
       c[con[0]]=dict(port=con[1],proto=con[2])
     self.connections=c
-    if config:config[1] = closePort(config[1])
-    self.config = config
-    if SERVICE=="ngrok":self.ngrok=ngrok(TOKEN,USE_FREE_TOKEN,connections,region,self.config)
+    self.ngrok=ngrok(TOKEN,USE_FREE_TOKEN,connections,region,config)
     self.SERVICE = SERVICE
-    
 
   def start(self,name,btc='b',displayB=True,v=True):
     from IPython.display import clear_output
@@ -664,66 +525,17 @@ class PortForward:
               clear_output()
               loadingAn(name="lds")
               textAn("Starting localhost...", ty="twg")
-          data = dict(url="https://"+LocalhostRun(port).keep_alive())
+          data = dict(url="http://"+LocalhostRun(port).keep_alive())
           if displayB:
               displayUrl(data, btc)
           return data
     elif self.SERVICE == "ngrok":
         return self.ngrok.start(name,btc,displayB,v)
-    elif self.SERVICE == "argotunnel":
-        con=self.connections[name]
-        port=con["port"]
-        proto=con["proto"]
-        if v:
-          clear_output()
-          loadingAn(name="lds")
-          textAn("Starting argo tunnel...", ty="twg")
-        data = dict(url="https://"+ArgoTunnel(port, proto, closePort(self.config[1])).keep_alive())
-        if displayB:
-          displayUrl(data, btc)
-        return data
-    elif self.SERVICE == "jprq":
-        con=self.connections[name]
-        port=con["port"]
-        proto=con["proto"]
-        if v:
-          clear_output()
-          loadingAn(name="lds")
-          textAn("Starting jprq...", ty="twg")
-        data = dict(url="https://"+jprq(port, proto).keep_alive())
-        if displayB:
-          displayUrl(data, btc)
-        return data
-        
+
 
 class PortForward_wrapper(PortForward):
   def __init__(self,SERVICE,TOKEN,USE_FREE_TOKEN,connections,region,config):
     super(self.__class__,self).__init__(connections,region,SERVICE,TOKEN,USE_FREE_TOKEN,config)
-
-
-def findPackageR(id_repo, p_name, tag_name=False, all_=False):
-  import requests
-
-  for rawData in requests.get(f"https://api.github.com/repos/{id_repo}/releases").json():
-    if tag_name:
-      if rawData['tag_name'] != tag_name:
-        continue
-
-    for f in rawData['assets']:
-      if p_name == f['browser_download_url'][-len(p_name):]:
-        rawData['assets'] = f 
-        return f['browser_download_url'] if not all_ else rawData
-  raise Exception("Unable to call the API!\n Try again with change packages name")
-
-def closePort(port):
-  import socket
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    for _ in range(100):
-      if s.connect_ex(('localhost', port)) == 0:
-        port += 1
-      else:
-        return port
-  raise Exception("Unable to close the port!")
 
 # ====================================================================================================
 def installJDownloader():
